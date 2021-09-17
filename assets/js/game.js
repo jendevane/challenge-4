69f4320495fbfaeda630a228b0510db53e8d8bcf
jendevane <87095302+jendevane@users.noreply.github.com>
2021-09-17 18:35:55 -0400

    error

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,8 +3,6 @@ const choices = Array.from(document.getElementsByClassName('choice-text'));
 const progressText = document.getElementById('progressText');
 const scoreText = document.getElementById('score');
 const progressBarFull = document.getElementById('progressBarFull');
-const loader = document.getElementById('loader');
-const game = document.getElementById('game');
 let currentQuestion = {};
 let acceptingAnswers = false;
 let score = 0;
@@ -18,26 +16,7 @@ fetch('questions.json')
         return res.json();
     })
     .then((loadedQuestions) => {
-        questions = loadedQuestions.results.map((loadedQuestion) => {
-            const formattedQuestion = {
-                question: loadedQuestion.question,
-            };
-
-            const answerChoices = [...loadedQuestion.incorrect_answers];
-            formattedQuestion.answer = Math.floor(Math.random() * 4) + 1;
-            answerChoices.splice(
-                formattedQuestion.answer - 1,
-                0,
-                loadedQuestion.correct_answer
-            );
-
-            answerChoices.forEach((choice, index) => {
-                formattedQuestion['choice' + (index + 1)] = choice;
-            });
-
-            return formattedQuestion;
-        });
-
+        questions = loadedQuestions;
         startGame();
     })
     .catch((err) => {
@@ -53,8 +32,6 @@ startGame = () => {
     score = 0;
     availableQuesions = [...questions];
     getNewQuestion();
-    game.classList.remove('hidden');
-    loader.classList.add('hidden');
 };
 
 getNewQuestion = () => {
