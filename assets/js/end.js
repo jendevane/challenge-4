65ff935956d80e2b497f82c4e56314d225a0edeb
jendevane <87095302+jendevane@users.noreply.github.com>
2021-09-17 21:05:34 -0400

    added files

--- assets/js/end.js
+++ assets/js/end.js
@@ -1,8 +1,12 @@
-
 const username = document.getElementById('username');
 const saveScoreBtn = document.getElementById('saveScoreBtn');
 const finalScore = document.getElementById('finalScore');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
+
+const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+
+const MAX_HIGH_SCORES = 5;
+
 finalScore.innerText = mostRecentScore;
 
 username.addEventListener('keyup', () => {
@@ -16,10 +20,10 @@ saveHighScore = (e) => {
         score: mostRecentScore,
         name: username.value,
     };
-    highScore.push(score);
-    highScore.sort((a, b) => b.score - a.score);
-    highscore.splice(5);
+    highScores.push(score);
+    highScores.sort((a, b) => b.score - a.score);
+    highScores.splice(5);
 
-    localStorage.setItem('highScores', JSON.stringify(highsScores));
+    localStorage.setItem('highScores', JSON.stringify(highScores));
     window.location.assign('/');
 };
